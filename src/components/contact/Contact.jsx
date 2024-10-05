360d16f268e24345ab2832aab4259ccc5df20416
ShahadathAlam <shahadathalam@ymail.com>
2024-10-06 04:35:46 +0600

    Contact Component has been finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,108 @@
 import React from "react";
 import "./contact.css";
+
+import { MdEmail } from "react-icons/md";
+
+import { FaFacebookMessenger } from "react-icons/fa";
+
+import { IoLogoWhatsapp } from "react-icons/io5";
+
+import { useRef } from "react";
+// import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_1xgpf3b", "template_wi3a72t", form.current, {
+        publicKey: "_gOoh2vJjVnWotY9D",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>shahadathalam@ymail.com</h5>
+            <a
+              href="mailto:shahadathalam@ymail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <FaFacebookMessenger className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Shahadath alam</h5>
+            <a
+              href="https://m.me/MdShahadathAlamOfficial"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <IoLogoWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+8801837130481</h5>
+            <a
+              href="https://wa.me/8801837130481"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        {/* END OF CONTACT OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
